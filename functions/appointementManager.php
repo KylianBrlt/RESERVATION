5e476c96d692cc8d0237ed9e24fd57b832316a6e
Kylian BRAULT <kbrault@et.esiea.fr>
2025-02-24 22:11:43 +0100

    move file

--- functions/appointementManager.php
+++ functions/appointementManager.php
@@ -1,4 +1,9 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once 'getPDO.php';
 
 function createAppointment($user_email, $date, $time) {
